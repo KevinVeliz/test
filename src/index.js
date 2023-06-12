fe8f0cf12b20bb1bffa0b8367bd1e6800c94375c
KevinVeliz <mario-veliz258934@hotmail.com>
2023-06-12 15:35:16 -0500

    Nose

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require('./server.js')
+require('dotenv').config()
+
+const PORT= process.env.PORT || 3000
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server on port ${PORT}`)
+})
