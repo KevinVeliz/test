70f645451d77710df0f0c950b19e10464b2e64d8
KevinVeliz <mario-veliz258934@hotmail.com>
2023-06-12 15:52:33 -0500

    cambio puertos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const app = require('./server.js')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000
+
 
 app.listen(app.get('port'),()=>{
-    console.log(`Server on port ${PORT}`)
+    console.log(`Server on port ${app.get('port')}`)
 })
